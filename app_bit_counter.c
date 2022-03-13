87a0ef8cb7ec29fe997314cab043aa842124efa0
mafuzalhoque <mafuzalhoque@gmail.com>
2022-03-12 22:35:53 -0500

    FiniteStateMachine3_12

--- app_bit_counter.c
+++ app_bit_counter.c
@@ -44,4 +44,32 @@ int main(int argc, char **argv)
 {
     fsm_t *fsm = create_new_fsm("Bit Flipper");
     state_t *state_S0 = create_new_state("S0",FSM_TRUE);
+    set_fsm_initial_state(fsm, state_S0);
+
+    char bit = '0';
+    create_and_insert_new_tt_entry(&state_S0->state_trans_table,
+                                  &bit, 1, 
+                                  bit_flipper_fn_gen,
+                                  state_S0);
+    
+    bit = '1';
+    create_and_insert_new_tt_entry(&state_S0->state_trans_table,
+                                  &bit, 1, 
+                                  bit_flipper_fn_gen,
+                                  state_S0);
+
+
+    fsm_bool_t fsm_result;
+    fsm_error_t fsm_error;
+
+    fsm_error = execute_fsm(fsm,
+                            "0000000\0",
+                            strlen("0000000\0"),
+                            0,
+                            &fsm_result);
+
+    if(fsm_error == FSM_NO_ERROR)
+    {
+        
+    }
 }
